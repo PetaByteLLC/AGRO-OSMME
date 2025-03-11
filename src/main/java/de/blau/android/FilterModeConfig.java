09e831300824adca6c8248c513524039d3e30eb3
tariel27 <tariel.fifa2015@gmail.com>
2025-03-11 13:04:34 +0600

    updated

--- src/main/java/de/blau/android/FilterModeConfig.java
+++ src/main/java/de/blau/android/FilterModeConfig.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.content.Context;
+import android.location.Location;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import de.blau.android.filter.Filter;
@@ -32,7 +34,19 @@ public class FilterModeConfig implements ModeConfig {
 
     @Override
     public HashMap<String, String> getExtraTags(@NonNull Logic logic, @NonNull OsmElement e) {
-        return null;
+        HashMap<String, String> map = new HashMap<>();
+        Map logicMap = logic.getMap();
+        if (logicMap == null) return null;
+        if (logicMap.getTracker() == null) return null;
+        Location lastLocation = logicMap.getTracker().getLastLocation();
+        if (lastLocation == null) return null;
+        String position = e.getTagWithKey("position");
+        if (position != null) return null;
+        String newPosition = String.format("%s, %s;", lastLocation.getLatitude(), lastLocation.getLongitude());
+//        map.put("position", position != null ?
+//                (Objects.equals(newPosition, position) ? position : newPosition + position) : newPosition);
+        map.put("position", newPosition);
+        return map;
     }
 
     @Override
